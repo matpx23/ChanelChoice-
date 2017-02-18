bf74be045f3244b8899db1d38757b5e53a0f9592
matt paixao <mat.paixao@contactpartners.com>
2017-02-18 21:07:25 +0000

    activity page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 import { NavController } from 'ionic-angular';
 
 @Component({
@@ -7,9 +6,16 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  graph;
   constructor(public navCtrl: NavController) {
-
+  /*this.graph.data = [
+      //Awake
+      [16, 15, 20, 12, 16, 12, 8],
+      //Asleep
+      [8, 9, 4, 12, 8, 12, 14]
+    ];
+    this.graph.labels = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+    this.graph.series = ['In', 'Out'];*/
   }
 
 }
